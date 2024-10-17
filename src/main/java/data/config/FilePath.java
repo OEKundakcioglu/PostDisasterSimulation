b7944fae0803ed0514c6d2a59a91a469cc3741b7
Cem Yarkın Yıldız <yarkin.yildiz@ozu.edu.tr>
2024-10-17 16:25:29 +0300

    input file name has been changed

--- src/main/java/data/config/FilePath.java
+++ src/main/java/data/config/FilePath.java
@@ -2,6 +2,6 @@ package data.config;
 
 public class FilePath {
     public static final String RANDOM_CONFIG = "src/main/java/data/input_files/randomConfig.yaml";
-    public static final String INPUT_CONFIG = "src/main/java/data/input_files/azizi.yaml";
+    public static final String INPUT_CONFIG = "src/main/java/data/input_files/input.yaml";
     public static boolean isReadFromInputFile = true;
 }
